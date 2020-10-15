5f5dfc71b40e5652a7ade4a9568172c4bdd894f2
Chex-Z <zambonojutsu@gmail.com>
2020-10-15 22:31:42 +0000

    base insurance and account templates

--- p_portal/urls.py
+++ p_portal/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
 path("", views.patient_home_view, name="patient_home_view"),
+path("patient_insurance", views.patient_insurance_view, name="patient_insurance_view"),
+path("patient_account", views.patient_account_view, name="patient_account_view"),
 ]
--- p_portal/views.py
+++ p_portal/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def patient_home_view(request):
     return render(request, "patient_home_template.html", {})
+
+def patient_account_view(request):
+    return render(request, "account_template.html", {})
+
+def patient_insurance_view(request):
+    return render(request, "insurance_template.html", {})
+
